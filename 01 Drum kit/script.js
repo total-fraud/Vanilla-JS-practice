e95876d9411749c20a36fa175da537319227b262
total-fraud <lumen437@gmail.com>
2020-09-01 21:13:55 +0400

    All works, left to add style

---
+++ 01 Drum kit/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('keydown', function(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    if (!audio) return; 
+    audio.currentTime = 0;
+    audio.play();
+});
